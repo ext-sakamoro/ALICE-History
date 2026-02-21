d4d4cf15642903dd0b39f4dc5727e3c302574189
Moroya Sakamoto <noreply@ext-sakamoro.dev>
2026-02-21 12:38:34 +0900

    chore: Add .gitignore and remove target/ build artifacts

--- target/debug/deps/alice_history-17aa770edc85aa12.d
+++
@@ -1,7 +0,0 @@
-/Users/ys/ALICE-History/target/debug/deps/alice_history-17aa770edc85aa12.d: src/lib.rs
-
-/Users/ys/ALICE-History/target/debug/deps/libalice_history-17aa770edc85aa12.rlib: src/lib.rs
-
-/Users/ys/ALICE-History/target/debug/deps/libalice_history-17aa770edc85aa12.rmeta: src/lib.rs
-
-src/lib.rs:
--- target/debug/deps/alice_history-47048d59f028582e.d
+++
@@ -1,5 +0,0 @@
-/Users/ys/ALICE-History/target/debug/deps/alice_history-47048d59f028582e.d: src/lib.rs
-
-/Users/ys/ALICE-History/target/debug/deps/alice_history-47048d59f028582e: src/lib.rs
-
-src/lib.rs:
